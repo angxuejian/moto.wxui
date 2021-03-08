197b082e7ef93f3cc98f0601add89f1e1925ff43
angxuejian <xuejian.ang@gmail.com>
2021-03-08 21:03:47 +0800

    chore: diff cloud env

--- All.ui/cloudfunctions/getRemoveBG/index.js
+++ All.ui/cloudfunctions/getRemoveBG/index.js
@@ -1,7 +1,11 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
 const $ = require('request')
-cloud.init()
+cloud.init(
+  {
+    env: cloud.DYNAMIC_CURRENT_ENV
+  }
+)
 
 // 云函数入口函数
 exports.main = async (event, context) => {
