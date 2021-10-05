8b344e1952cd689537bde25a257c4cddf459e248
angxuejian <xuejian.ang@gmail.com>
2021-10-06 00:08:09 +0800

    refactor: edit canvas related code in ui.picCrop

--- All.ui/miniprogram/pages/UI-Pages/ui.picCrop/ui.picCrop.js
+++ All.ui/miniprogram/pages/UI-Pages/ui.picCrop/ui.picCrop.js
@@ -15,17 +15,18 @@ Page({
       startY: 0,
       moveX : 0,
       moveY : 0,
-    }
+    },
+    cropNode: null,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
-    // this.initImgSrc('../../../assets/b.jpg')
 
-    this.initImgSrc('https://tse1-mm.cn.bing.net/th/id/R-C.d85e977b14b92fa4ce392537daa35f92?rik=T1Lg%2fMbgJV3XFw&riu=http%3a%2f%2f5b0988e595225.cdn.sohucs.com%2fimages%2f20190831%2f59f17a0fc1fa48a98037725de23a602b.jpeg&ehk=oc55CzaOGkMnkhy09TPTSw1HsXY%2bjPVhwLT5kcjE77g%3d&risl=&pid=ImgRaw&r=0')
+    const src = 'https://images.pexels.com/photos/5002528/pexels-photo-5002528.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+
+    this.initImgSrc(src)
 
   },
 
@@ -33,6 +34,7 @@ Page({
   initImgSrc: async function(src) {
 
     const img = await wx.getImageInfo({ src })
+
     const sys = await wx.getSystemInfo()
 
     const wh = sys.windowWidth * 0.8
@@ -43,9 +45,7 @@ Page({
       this.data.boxSize.h = wh
       boxd.boxSize = this.data.boxSize
     }
-
     const { boxSize, imgSize } = this.data
-
 
     // 获取图片的宽高
     let list = []
@@ -61,6 +61,7 @@ Page({
     // 先将图片大小、绘制canvas标签上
     this.setData({ imgSize, ...boxd })
 
+
     // 获取图片的位置
     const diff = {
       sysX: (sys.windowWidth  - boxSize.w) / 2,
@@ -69,24 +70,23 @@ Page({
       imgY: (sys.windowHeight - imgSize.height) / 2,
     }
 
-    imgSize.src = img.path
+    imgSize.initSrc = img.path // 背景图片、使用已加载到本地的图片
+    imgSize.cropSrc = src      // 裁剪时、使用线上地址图片
     imgSize.x = diff.imgX - diff.sysX
     imgSize.y = diff.imgY - diff.sysY
     imgSize.pixelRatio = sys.pixelRatio
-
-  
 
     // 初始化 - 按照比例缩放图片
     // 将图片缩放至裁剪区域
     const d = {
       x: 0, y: 0,
-      src   : imgSize.src,
+      src   : imgSize.initSrc,
       width : imgSize.width,
-      height: imgSize.height
+      height: imgSize.height,
+      dpr   : imgSize.pixelRatio
     }
  
-    const imgSrc = await this.drawImgSrc(d, 'init')
-
+    const imgSrc = await this.drawInitImgSrc(d)
     this.setData({ imgSrc })
   },
 
@@ -100,21 +100,76 @@ Page({
     const d = {
       x: imgSize.x + this.data.x, 
       y: imgSize.y + this.data.y,
-      src   : imgSize.src,
+      src   : imgSize.cropSrc,
       width : imgSize.width,
-      height: imgSize.height
+      height: imgSize.height,
+      dpr   : imgSize.pixelRatio
     }
-
-    console.log(d, '这是')
-    this.getCanvas(d)
-    wx.hideLoading()
-
-    return
-    const src = await this.drawImgSrc(d, 'crop')
+  
+    const src = await this.drawCropImgSrc(d)
     wx.previewImage({ urls: [src] })
-    this.setData({imgshow: src })
     wx.hideLoading()
   },
+
+
+  // 裁剪图片
+  // 裁剪图片时没办法使用本地缓存图片
+  drawCropImgSrc: async function(data) {
+    const { dpr, x, y, width, height, src } = data
+    let crop = this.data.cropNode
+
+    if (!crop) {
+      crop = await this.getCanvasNode()
+      this.data.cropNode = crop
+    }
+    
+    return new Promise((resolve) => {
+      const canvas = crop.node
+      const ctx = canvas.getContext('2d')
+
+      canvas.width = crop.width * dpr
+      canvas.height = crop.height * dpr
+      ctx.scale(dpr, dpr)
+
+      ctx.fillStyle = '#fff'
+      ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+      const img = canvas.createImage()
+      img.onload = () => {
+        ctx.drawImage(img,x,y, width, height)
+        resolve(canvas.toDataURL('image/png', 1))
+      }
+      img.src = src
+    })
+
+  },
+
+  // 绘制待裁剪背景图片
+  drawInitImgSrc: function(data, id = 'init') {
+    return new Promise((resolve) => {
+      const { x, y, width, height, src, dpr } = data
+      const ctx = wx.createCanvasContext(id)
+      ctx.drawImage(src, x, y, width, height)
+
+      ctx.draw(false, () => {
+        setTimeout(() => {
+          wx.canvasToTempFilePath({
+            x,  y, width, height, 
+            canvasId: id,
+            destWidth: width * dpr,
+            destHeight: height * dpr,
+            success: res => {
+              resolve(res.tempFilePath)
+            },
+            fail: err => {
+              console.log(err, '这是')
+            }
+          })
+        }, 200)
+      })
+    })
+  },
+
 
   // start touch
   onTouchStart: function(event) {
@@ -135,9 +190,7 @@ Page({
     const x = this.data.touch.moveX - this.data.touch.startX
     const y = this.data.touch.moveY - this.data.touch.startY
 
-    this.setData({
-      x, y
-    })
+    this.setData({ x, y })
   },
 
   // end touch
@@ -146,73 +199,19 @@ Page({
   },
 
 
-  getCanvas: function(data) {
-    const { x, y, width, height, src } = data
-    wx.createSelectorQuery()
-    .select('#crop')
-    .fields({
-      node: true,
-      size: true,
-    })
-    .exec(res => {
-      console.log(res, "这是")
-      const canvas = res[0].node
-      const ctx = canvas.getContext('2d')
-
-      const dpr = wx.getSystemInfoSync().pixelRatio
-      canvas.width = res[0].width * dpr
-      canvas.height = res[0].height * dpr
-      ctx.scale(dpr, dpr)
-
-      ctx.fillStyle = '#fff'
-      ctx.fillRect(0, 0, canvas.width, canvas.height)
-      const img = canvas.createImage()
-      img.onload = () => {
-        ctx.drawImage(img, x, y, width, height)
-        const url = canvas.toDataURL('jpg', 1)
-        wx.previewImage({ urls: [url] })
-      }
-      img.src = 'https://tse1-mm.cn.bing.net/th/id/R-C.d85e977b14b92fa4ce392537daa35f92?rik=T1Lg%2fMbgJV3XFw&riu=http%3a%2f%2f5b0988e595225.cdn.sohucs.com%2fimages%2f20190831%2f59f17a0fc1fa48a98037725de23a602b.jpeg&ehk=oc55CzaOGkMnkhy09TPTSw1HsXY%2bjPVhwLT5kcjE77g%3d&risl=&pid=ImgRaw&r=0'
-
-      
-    })
-  },
-
-
-  // 绘制图片
-  drawImgSrc: function(data, canvasId) {
-
+  getCanvasNode: function(id = 'crop') {
     return new Promise((resolve) => {
-      const { x, y, width, height, src } = data
-
-      const ctx = wx.createCanvasContext(canvasId)
-
-      if (canvasId === 'crop') {
-        ctx.fillStyle = 'red'
-        ctx.fillRect(0, 0,this.data.boxSize.w, this.data.boxSize.h)
-        // ctx.draw()
-        ctx.drawImage(src, x, y, width, height)
-
-      } else {
-        ctx.drawImage(src, x, y, width, height)
-
-      }
-
-    
-      ctx.draw(false, () => {
-        wx.canvasToTempFilePath({
-          x,  y, width, height, canvasId,
-          success: res => {
-            resolve(res.tempFilePath)
-          },
-          fail: err => {
-            console.log(err, 'canvas 导出失败')
-          }
+      wx.createSelectorQuery()
+        .select('#' + id)
+        .fields({
+          node: true,
+          size: true,
         })
-      })
+        .exec(res => {
+          resolve(res[0])
+        })
     })
   },
-
 
   getWidthFix: function(width, height, styleW, styleH) {
 
