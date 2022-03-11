fde479f60bb380a900c6f44618aec9aa0359f24f
angxuejian <xuejian.ang@gmail.com>
2022-03-11 20:14:55 +0800

    feat: complete ui.electronicSignature page

--- All.ui/miniprogram/pages/UI-Pages/ui.electronicSignature/ui.electronicSignature.js
+++ All.ui/miniprogram/pages/UI-Pages/ui.electronicSignature/ui.electronicSignature.js
@@ -1,11 +1,16 @@
 // pages/UI-Pages/ui.electronicSignature/ui.electronicSignature.js
+
+let canvas = null
+let ctx = null
+let dpr = null
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    touchArr: [],
+    isDraw: false
   },
 
   /**
@@ -18,7 +23,102 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady() {
+    this.initCanvas()
+  },
+
+  initCanvas: function () {
+    const query = wx.createSelectorQuery()
+    query.select('#myCanvas')
+      .fields({
+        node: true,
+        size: true
+      })
+      .exec((res) => {
+        canvas = res[0].node
+        ctx = canvas.getContext('2d')
+        dpr = wx.getSystemInfoSync().pixelRatio
+        canvas.width = res[0].width * dpr
+        canvas.height = res[0].height * dpr
+        ctx.scale(dpr, dpr)
+        ctx.lineWidth = 4
+        ctx.shadowColor = '#000000'
+        ctx.shadowBlur = 1
+        ctx.lineCap = 'round'
+        ctx.lineJoin = 'round'
+        ctx.strokeStyle = '#000000'
+        this.data.touchArr = []
+        this.clearCanvas()
+      })
+  },
+
+  clearCanvas: function() {
+    ctx.clearRect(0, 0, canvas.width * dpr, canvas.height * dpr)
+  
+    ctx.fillStyle = '#ffffff'
+    ctx.fillRect(0, 0, canvas.width * dpr, canvas.height * dpr)
+  },
+  drawElSignature: function () {
+    if (!this.data.touchArr.length || !this.data.isDraw) return
+
+    this.clearCanvas()
+    ctx.beginPath()
+    for (let i = 0; i < this.data.touchArr.length; i++) {
+      const item = this.data.touchArr[i]
+      if (item.z === 0) {
+        ctx.moveTo(item.x, item.y)
+      } else {
+        ctx.lineTo(item.x, item.y)
+      }
+    }
+    ctx.stroke()
+  },
+
+  onTouchStart: function (event) {
+    this.data.isDraw = true
+    this.data.touchArr.push({
+      x: event.changedTouches[0].pageX - event.target.offsetLeft,
+      y: event.changedTouches[0].pageY - event.target.offsetTop,
+      z: 0
+    })
+    this.drawElSignature()
+  },
+
+  onTouchMove: function (event) {
+
+    this.data.touchArr.push({
+      x: event.changedTouches[0].pageX - event.target.offsetLeft,
+      y: event.changedTouches[0].pageY - event.target.offsetTop,
+      z: 1
+    })
+    this.drawElSignature()
+  },
+
+  onTouchEnd: function (event) {
+    this.data.isDraw = false
+    this.data.touchArr.push({
+      x: event.changedTouches[0].pageX - event.target.offsetLeft,
+      y: event.changedTouches[0].pageY - event.target.offsetTop,
+      z: 0
+    })
+  },
+  reset: function () {
+    this.initCanvas()
+  },
+  config: function () {
+
+    wx.canvasToTempFilePath({
+      canvas: canvas,
+      width: canvas.width / dpr,
+      height: canvas.height / dpr,
+      destWidth: canvas.width,
+      destHeight: canvas.height,
+      success: res => {
+        wx.previewImage({
+          urls: [res.tempFilePath],
+        })
+      }
+    })
 
   },
 
