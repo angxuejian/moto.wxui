3013e171d13bef69467d1930ae601e91ee2dda65
angxuejian <xuejian.ang@gmail.com>
2022-03-19 17:28:01 +0800

    feat: add random lottery code in the ui.lottery page

--- All.ui/miniprogram/pages/UI-Pages/ui.lottery/ui.lottery.js
+++ All.ui/miniprogram/pages/UI-Pages/ui.lottery/ui.lottery.js
@@ -5,16 +5,65 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    prizes: [],
+    nameArr: [],
+    weightSum: 0,
+    weightArr: [],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.initPrize()
   },
 
+  initPrize: function() {
+    this.data.prizes = [
+      { name: '一等奖', percent: 0.01, count: 1 },
+      { name: '二等奖', percent: 0.05, count: 3 },
+      { name: '三等奖', percent: 0.14, count: 10 },
+      { name: '未中奖', percent: 0.80, count: -1 }
+    ]
+    this.data.nameArr = []
+    this.data.weightSum = 0
+    this.data.weightArr = []
+
+    this.data.prizes.forEach(item => {
+      const weight = item.percent
+      this.data.weightSum += weight
+      this.data.weightArr.push(weight)
+      this.data.nameArr.push(item.name)
+    })
+  },
+  getLottery: function() {
+    const { prizes, weightArr, nameArr, weightSum } = this.data
+    
+    const random = Math.random() * weightSum
+    weightArr.push(random)
+
+    const sortWeightArr = weightArr.sort((a, b) => a - b )
+    const index = Math.min(sortWeightArr.indexOf(random), prizes.length - 1)
+
+    const itemPrize = prizes[index]
+    let tip = nameArr[index]
+
+    if (itemPrize.count >= 1) itemPrize.count--
+    else if (itemPrize.count === 0) { 
+      tip += '已被领取完 - 未中奖'
+    }
+
+    console.log(tip)
+  },
+
+  testWhile: function() {
+    this.initPrize()
+
+    for (let i = 0; i < 100; i++) {
+      this.getLottery()
+    }
+  },
+  
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
